058e0feb07a7687c303ce69494a93576be3f41bd
OL99-ols <68142050+OL99-ols@users.noreply.github.com>
2020-07-11 15:13:41 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -47,22 +47,22 @@ let Peach = sprites.create(img`
 `, SpriteKind.picture)
 Peach.setPosition(999, 999)
 let World_list = [sprites.create(img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
+4 d d d d d d d d f 4 d d d d 4 
+d 4 4 4 4 4 4 4 4 f d 4 4 4 4 f 
+d 4 4 4 4 4 4 4 4 f d 4 4 4 4 f 
+d 4 4 4 4 4 4 4 4 f d 4 4 4 4 f 
+d 4 4 4 4 4 4 4 4 f d f 4 4 4 f 
+d 4 4 4 4 4 4 4 4 f 4 f f f f 4 
+d 4 4 4 4 4 4 4 4 f d d d d d f 
+d 4 4 4 4 4 4 4 4 f d 4 4 4 4 f 
+d 4 4 4 4 4 4 4 4 f d 4 4 4 4 f 
+d 4 4 4 4 4 4 4 4 f d 4 4 4 4 f 
+d f 4 4 4 4 4 4 f d 4 4 4 4 4 f 
+d d f f 4 4 4 4 f d 4 4 4 4 4 f 
+d 4 d d f f f f d 4 4 4 4 4 4 f 
+d 4 4 4 d d d f d 4 4 4 4 4 4 f 
+d 4 4 4 4 4 4 f d 4 4 4 4 4 f f 
+4 f f f f f f 4 d f f f f f f 4 
 `, SpriteKind.Map), 2, 0, 0]
 forever(function () {
 	
